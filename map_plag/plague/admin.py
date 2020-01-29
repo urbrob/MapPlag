b22899aaea156f8dcb352ae4921c9c8299f8276f
Daria Lasecka <33495679+darialasecka@users.noreply.github.com>
2020-01-30 00:17:25 +0100

    Created KeywordTag model, admin model, and made migrations (#17)

--- map_plag/plague/admin.py
+++ map_plag/plague/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Informer
+from .models import Informer, KeywordTag
 
 
 @admin.register(Informer)
 class InformerAdmin(admin.ModelAdmin):
 	list_display = ['api_key', 'lat', 'lan', 'street', 'street_number', 'city', 'name']
+
+
+@admin.register(KeywordTag)
+class KeywordTagAdmin(admin.ModelAdmin):
+	list_display = ['name', 'type']
