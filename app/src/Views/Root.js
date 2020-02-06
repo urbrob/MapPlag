220d217b0365b9e8ceabbe96feef5d8f8e477731
dawidcwiek <cwiekdawid1@gmail.com>
2020-02-06 16:01:40 +0100

    add eslint husky prettier and styled component

--- app/src/Views/Root.js
+++ app/src/Views/Root.js
@@ -1,23 +1,11 @@
-import React from "react";
-import logo from "logo.svg";
+import React from 'react';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <>
+    <GlobalStyle />
+    <h1>Here will be the map</h1>
+  </>
 );
 
 export default Root;
