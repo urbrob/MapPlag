cea59fb8045dccc99e3dd74cb540afb544a78bb1
Daria Lasecka <daria.las@wp.pl>
2020-02-06 13:36:58 +0100

    Changed url so it contains api

--- map_plag/map_plag/urls.py
+++ map_plag/map_plag/urls.py
@@ -22,6 +22,6 @@ from plague.views import KeywordViewSet
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('^keywords/', KeywordViewSet.as_view()),
+    url('^api/keywords/', KeywordViewSet.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
